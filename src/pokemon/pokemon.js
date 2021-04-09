deee49d875f23d345a559367865c217b3ccacd69
Rivaldo Esteban <63760024+RivaldoEsteban@users.noreply.github.com>
2021-04-09 16:00:05 -0500

    actulizando clases de la pokedex

--- src/pokemon/pokemon.js
+++ src/pokemon/pokemon.js
@@ -11,24 +11,22 @@ async function getPokemon(endpoint, id) {
 
 function CardPokemon() {
   const [pokemon, setPokemon] = useState({
-    name: "totodile",
-    imageFront:
-      "https://assets.pokemon.com/assets/cms2/img/pokedex/full/158.png",
+    name: "",
+    imageFront: "",
     imageBack: "",
-    id: "169",
+    id: "",
     types: [],
     abilities: [],
   });
 
   async function handleSubmit(event) {
     event.preventDefault();
+    const pokeScreen = document.querySelector("#poke-screen");
+    pokeScreen.style.background = "white";
     const value = event.target.querySelector("input").value;
-
     const pokemon = await getPokemon("pokemon", value);
-
     const speech = window.speechSynthesis;
     const otherData = await getPokemon("pokemon-species", value);
-    // console.log(otherData);
 
     const description = otherData.flavor_text_entries.find(
       (flavor) => flavor.language.name === "es"
@@ -50,49 +48,50 @@ function CardPokemon() {
   }
 
   return (
-    <div className="pokedex">
-      <div>
-        <p className="circle-icon"></p>
-        <div className="icon">
+    <div className="wrapper">
+      <div className="pokedex">
+        <div className="top-circles">
+          <p className="circle-icon"></p>
           <p className="icon-red"></p>
-          <p className="icopn-"></p>
+          <p className="icon-yellow"></p>
         </div>
-      </div>
-      <div className="information-pokemon">
-        <div className="image-pokemon">
-          <h1 className="name-pokemon">{pokemon.name}</h1>
-          <div className="image-group">
-            <img className="image" src={pokemon.imageFront} alt="" />
-            <img className="image" src={pokemon.imageBack} alt="" />
-          </div>
-          <div className="types-pokemon">
-            {pokemon.types.map((type) => {
-              return <p className="type">{type.type.name}</p>;
-            })}
-          </div>
-        </div>
-        <div className="data-pokemon">
-          <div className="data">
-            <h4 className="datatype">abilities</h4>
-            <div className="data-result">
-              {pokemon.abilities.map((ability) => {
-                return <li className="abilities">{ability.ability.name}</li>;
-              })}
+        <div className="information-pokemon">
+          <div className="pokemon-container">
+            <div className="image-pokemon" id="poke-screen">
+              <h1 className="name-pokemon">{pokemon.name}</h1>
+              <div className="image-group">
+                <img className="image" src={pokemon.imageFront} alt="" />
+                <img className="image" src={pokemon.imageBack} alt="" />
+              </div>
+              {/* <div className="types-pokemon">
+                {pokemon.types.map((type) => {
+                  return <p className="type">{type.type.name}</p>;
+                })}
+              </div> */}
             </div>
           </div>
+          {/* <div className="data-pokemon">
+            <div className="data">
+              <h4 className="datatype">abilities</h4>
+              <div className="data-result">
+                {pokemon.abilities.map((ability) => {
+                  return <li className="abilities">{ability.ability.name}</li>;
+                })}
+              </div>
+            </div>
+          </div> */}
         </div>
-      </div>
-      <div className="search-pokemon-input">
-        <form action="" onSubmit={handleSubmit}>
-          <input
-            className="input"
-            type="text"
-            placeholder="Escoge entre los 898 pokemones"
-            autoComplete="off"
-            // onChange={handleChange}
-          />
-          <button>enviar</button>
-        </form>
+        <div className="search-pokemon-input">
+          <form className="form" action="" onSubmit={handleSubmit}>
+            <input
+              className="input"
+              type="text"
+              placeholder="Son 898 pokemones"
+              autoComplete="off"
+            />
+            <button className="btn">Buscar</button>
+          </form>
+        </div>
       </div>
     </div>
   );
