bf9a7841fe638bc19ff654b388fce3890375ec8e
Rivaldo Esteban <63760024+RivaldoEsteban@users.noreply.github.com>
2021-04-09 19:00:19 -0500

    pokedex

--- src/pokemon/pokemon.js
+++ src/pokemon/pokemon.js
@@ -49,48 +49,64 @@ function CardPokemon() {
 
   return (
     <div className="wrapper">
-      <div className="pokedex">
-        <div className="top-circles">
-          <p className="circle-icon"></p>
-          <p className="icon-red"></p>
-          <p className="icon-yellow"></p>
-        </div>
-        <div className="information-pokemon">
-          <div className="pokemon-container">
-            <div className="image-pokemon" id="poke-screen">
-              <h1 className="name-pokemon">{pokemon.name}</h1>
-              <div className="image-group">
-                <img className="image" src={pokemon.imageFront} alt="" />
-                <img className="image" src={pokemon.imageBack} alt="" />
+      <div className="pokedex-container">
+        <div className="pokedex">
+          <div className="top-circles">
+            <p className="circle-icon"></p>
+            <p className="icon-red"></p>
+            <p className="icon-yellow"></p>
+          </div>
+          <div className="information-pokemon">
+            <div className="pokemon-container">
+              <div className="image-pokemon" id="poke-screen">
+                <h1 className="name-pokemon">{pokemon.name}</h1>
+                {pokemon.imageFront ? (
+                  <div className="image-group">
+                    <img className="image" src={pokemon.imageFront} alt="" />
+                    <img className="image" src={pokemon.imageBack} alt="" />
+                  </div>
+                ) : (
+                  ""
+                )}
               </div>
-              {/* <div className="types-pokemon">
-                {pokemon.types.map((type) => {
-                  return <p className="type">{type.type.name}</p>;
-                })}
-              </div> */}
             </div>
           </div>
-          {/* <div className="data-pokemon">
-            <div className="data">
-              <h4 className="datatype">abilities</h4>
+          <div className="search-pokemon-input">
+            <form className="form" action="" onSubmit={handleSubmit}>
+              <input
+                className="input"
+                type="text"
+                placeholder="Son 898 pokemones"
+                autoComplete="off"
+              />
+              <button className="btn">Buscar</button>
+            </form>
+          </div>
+          <div className="decoration">
+            <p className="d-1"></p>
+            <p className="d-2"></p>
+            <p className="d-3"></p>
+          </div>
+          <div className="poke-info">
+            <div className="abilities-pokemon">
+              <h4>Habilidades del Pokemon</h4>
               <div className="data-result">
                 {pokemon.abilities.map((ability) => {
-                  return <li className="abilities">{ability.ability.name}</li>;
+                  return <li className="type">{ability.ability.name}</li>;
                 })}
               </div>
+              {pokemon.types ? (
+                <div className="types-pokemon">
+                  <h4>Tipo de pokemon</h4>
+                  {pokemon.types.map((type) => {
+                    return <li className="type">{type.type.name}</li>;
+                  })}
+                </div>
+              ) : (
+                ""
+              )}
             </div>
-          </div> */}
-        </div>
-        <div className="search-pokemon-input">
-          <form className="form" action="" onSubmit={handleSubmit}>
-            <input
-              className="input"
-              type="text"
-              placeholder="Son 898 pokemones"
-              autoComplete="off"
-            />
-            <button className="btn">Buscar</button>
-          </form>
+          </div>
         </div>
       </div>
     </div>
